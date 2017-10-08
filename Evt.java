352f33dab1504253fb0ae11de3f4d2d1dba83cf9
Ibis Ibrahim <ibis@etu.unistra.fr>
2017-10-08 21:52:15 +0200

    Version finale

--- Evt.java
+++ Evt.java
@@ -13,15 +13,13 @@ public class Evt {
     protected double date;
     protected double date_debug; // Cas type depart
 
-    public Evt(int type, double date, int no_client)
-		{
+    public Evt(int type, double date, int no_client) {
         this.type = type;
         this.date = date;
         this.no_client = no_client;
     };
 
-    public Evt(int type, double date, int no_client, double date_arrive)
-		{
+    public Evt(int type, double date, int no_client, double date_arrive) {
         this.type = type;
         this.date = date;
         this.no_client = no_client;
@@ -30,10 +28,10 @@ public class Evt {
     };
 
     public void affichage() {
-            if (type == 0)
-                System.out.println("Date=" + date + "\t Depart  client #" + no_client + "\t arrive a t=" + date_debug);
-            else
-                System.out.println("Date=" + date + "\t Arrivee client #" + no_client);
+        if (type == 0)
+            System.out.println("Date=" + date + "\t Depart  client #" + no_client + "\t arrive a t=" + date_debug);
+        else
+            System.out.println("Date=" + date + "\t Arrivee client #" + no_client);
 
     }
 
